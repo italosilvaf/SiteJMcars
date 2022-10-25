e28a3b1391e3d887f69958645edff469f3030423
Italo SF <italosilvafernandesprog@gmail.com>
2022-10-25 12:10:39 -0300

    Responsividade do estoque pronto

--- JMcars/shop/models.py
+++ JMcars/shop/models.py
@@ -19,7 +19,7 @@ class Shop(models.Model):
 class Car(models.Model):
 
     modelo = models.CharField(
-        max_length=50, verbose_name='Modelo', default=None)
+        max_length=100, verbose_name='Modelo', default=None)
     marca = models.CharField(
         max_length=50, verbose_name='Marca', default=None)
     ano = models.CharField(max_length=9, verbose_name='Ano', default=None)
@@ -34,26 +34,8 @@ class Car(models.Model):
         max_length=50, verbose_name='Preço', default=None)
     publicado = models.BooleanField(
         verbose_name='Publicado', default=False)
-    imagem_um = models.ImageField(
+    imagem_capa = models.ImageField(
         upload_to='imagens_shop/imagens_carros', blank=False, null=False, default=None, verbose_name='Imagem 1 (Capa)')
-    imagem_dois = models.ImageField(
-        upload_to='imagens_shop/imagens_carros', blank=True, null=True, verbose_name='Imagem 2')
-    imagem_tres = models.ImageField(
-        upload_to='imagens_shop/imagens_carros', blank=True, null=True, verbose_name='Imagem 3')
-    imagem_quatro = models.ImageField(
-        upload_to='imagens_shop/imagens_carros', blank=True, null=True, verbose_name='Imagem 4')
-    imagem_cinco = models.ImageField(
-        upload_to='imagens_shop/imagens_carros', blank=True, null=True, verbose_name='Imagem 4')
-    imagem_seis = models.ImageField(
-        upload_to='imagens_shop/imagens_carros', blank=True, null=True, verbose_name='Imagem 6')
-    imagem_sete = models.ImageField(
-        upload_to='imagens_shop/imagens_carros', blank=True, null=True, verbose_name='Imagem 7')
-    imagem_oito = models.ImageField(
-        upload_to='imagens_shop/imagens_carros', blank=True, null=True, verbose_name='Imagem 8')
-    imagem_nove = models.ImageField(
-        upload_to='imagens_shop/imagens_carros', blank=True, null=True, verbose_name='Imagem 9')
-    imagem_dez = models.ImageField(
-        upload_to='imagens_shop/imagens_carros', blank=True, null=True, verbose_name='Imagem 10')
 
     def __str__(self):
         return self.modelo
