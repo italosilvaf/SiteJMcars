cbc439a92e7468b7fdcfde31d5dd97870e80bb89
ItaloSFnotebook <italosilvafernandesprog@gmail.com>
2023-01-19 14:30:12 -0300

    Categorias adicionadas no footer

--- about/views.py
+++ about/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import View
 from .models import About, Funcionario
-from categories.models import EstadoDeConservacao
+from categories.models import EstadoDeConservacao, Categoria
 
 
 class Index(View):
@@ -12,11 +12,13 @@ class Index(View):
         funcionarios = Funcionario.objects.filter(
             publicado=True).order_by('-id')
         estados_de_conservacao = EstadoDeConservacao.objects.all()
+        categorias = Categoria.objects.all()
 
         context = {
             'personalizacoes': personalizacoes,
             'funcionarios': funcionarios,
             'estados_de_conservacao': estados_de_conservacao,
+            'categorias': categorias,
         }
 
         return render(self.request, 'about/index.html', context)
--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from categories.models import EstadoDeConservacao
+from categories.models import EstadoDeConservacao, Categoria
 from .models import Home, QualidadesHome
 from django.views.generic import View
 from shop.models import Car
@@ -12,14 +12,16 @@ class Index(View):
             publicado_home=True).order_by('-id').first()
         cars = Car.objects.filter(
             publicado=True, destaque=True).order_by('-id')
-        estados_de_conservacao = EstadoDeConservacao.objects.all()
+        estados_de_conservacao = EstadoDeConservacao.objects.all()        
+        categorias = Categoria.objects.all()
         qualidades = QualidadesHome.objects.filter(
             publicado_qualidade=True)
 
         context = {
             'personalizacoes': personalizacoes,
             'cars': cars,
-            'estados_de_conservacao': estados_de_conservacao,
+            'estados_de_conservacao': estados_de_conservacao,            
+            'categorias': categorias,
             'qualidades': qualidades,
         }
 
